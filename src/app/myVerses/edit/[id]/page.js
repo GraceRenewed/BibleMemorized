9536153787f427b0c4c33512070b987d3dad1555
Christina Vieau <cvieau@live.com>
2024-11-19 21:15:58 -0500

    created collection and verse page.js folders

---
+++ src/app/myVerses/edit/[id]/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function editVerses() {
+  return <div>Edit Verses Form</div>;
+}
---
+++ src/app/myVerses/new/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function newVerse() {
+  return <div>Create New Verse Form</div>;
+}
